1b9ba1803055a9afe814988935f6468fd5a40705
Ismael Guimarães <ismael.guimaraes.borba@gmail.com>
2021-05-29 15:03:10 -0300

    feature: creation migration students

---
+++ src/database/migrations/1622310912019-CreateUsersTable.ts
@@ -0,0 +1,58 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateUsersTable1622310912019 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'students',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()'
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isNullable: false,
+                        isUnique: true
+                    },
+                    {
+                        name: 'telephone',
+                        type: 'varchar',
+                        isNullable: false,
+                        isUnique: true
+                    },
+                    {
+                        name: 'class',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('students');
+    }
+
+}
